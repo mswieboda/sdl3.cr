3e5296d31a638d3e2f1132a8da1182f56a716754
Matt Swieboda <mswieboda@users.noreply.github.com>
2026-02-14 21:08:19 -0600

    more TTF bindings, still WIP for full support

--- src/sdl3/gfx/ttf.cr
+++ src/sdl3/gfx/ttf.cr
@@ -1,5 +1,16 @@
 @[Link("SDL3_ttf")]
 lib LibSDL3TTF
+  alias Font = Void
+  alias TextEngine = Void
+  alias Text = Void
+
+  enum TTF_HorizontalAlignment : Int32
+    TTF_HORIZONTAL_ALIGN_INVALID = -1
+    TTF_HORIZONTAL_ALIGN_LEFT
+    TTF_HORIZONTAL_ALIGN_CENTER
+    TTF_HORIZONTAL_ALIGN_RIGHT
+  end
+
   fun init = TTF_Init() : Bool
   fun quit = TTF_Quit()
   fun was_init = TTF_WasInit() : Int32
@@ -7,8 +18,13 @@ lib LibSDL3TTF
   fun open_font_io = TTF_OpenFontIO(src : LibSDL3::IOStream*, closeio : Bool, ptsize : Float32) : Font*
   fun close_font = TTF_CloseFont(font : Font*)
   fun render_text_blended = TTF_RenderText_Blended(font : Font*, text : LibC::Char*, length : LibC::SizeT, fg : LibSDL3::Color) : LibSDL3::Surface*
-
-  alias Font = Void
+  fun render_text_blended_wrapped = TTF_RenderText_Blended_Wrapped(font : Font*, text : LibC::Char*, length : LibC::SizeT, fg : LibSDL3::Color, wrap_width : Int32) : LibSDL3::Surface*
+  fun create_text = TTF_CreateText(engine : TextEngine*, font : Font*, text : LibC::Char*, length : LibC::SizeT) : Text
+  fun create_renderer_text_engine = TTF_CreateRendererTextEngine(renderer : LibSDL3::Renderer*) : TextEngine
+  fun draw_renderer_text = TTF_DrawRendererText(text : Text*, x : Float32, y : Float32) : Bool
+  fun get_text_size = TTF_GetTextSize(text : Text*, w : LibC::Int*, y : LibC::Int*) : Bool
+  fun set_font_wrap = TTF_SetFontWrapAlignment(font : Font*, align : TTF_HorizontalAlignment) : Void
+  fun set_text_wrap_whitespace_visible = TTF_SetTextWrapWhitespaceVisible(text : Text*, visible : Bool) : Bool
 end
 
 module SDL3
@@ -34,8 +50,15 @@ module SDL3
       was_init != 1
     end
 
+    def create_renderer_text_engine(renderer : SDL3::Renderer) : TextEngine
+      LibSDL3TTF.create_renderer_text_engine(renderer.to_unsafe)
+    end
+
     class Font
       @ptr : LibSDL3TTF::Font*
+
+      def initialize(@ptr : LibSDL3TTF::Font*)
+      end
 
       def self.open(file : String, ptsize : Float32)
         ptr = LibSDL3TTF.open_font(file.to_unsafe, ptsize)
@@ -49,8 +72,6 @@ module SDL3
         new(ptr)
       end
 
-      def initialize(@ptr : LibSDL3TTF::Font*); end
-
       def close
         LibSDL3TTF.close_font(@ptr)
       end
@@ -59,10 +80,54 @@ module SDL3
         @ptr
       end
 
-      def render_text_blended(text : String, fg : LibSDL3::Color)
-        ptr = LibSDL3TTF.render_text_blended(@ptr, text.to_unsafe, text.bytesize, fg)
+      def render_text_blended(text : String, color : LibSDL3::Color) : Surface
+        ptr = LibSDL3TTF.render_text_blended(@ptr, text.to_unsafe, text.bytesize, color)
         raise "Failed to render text blended" if ptr.null?
         Surface.new(ptr)
+      end
+
+      def render_text_blended_wrapped(text : String, color : LibSDL3::Color, width : Int32 = 0) : Surface
+        ptr = LibSDL3TTF.render_text_blended_wrapped(@ptr, text.to_unsafe, text.bytesize, color, width)
+        raise "Failed to render text blended" if ptr.null?
+        Surface.new(ptr)
+      end
+
+      def create_text(engine : LibSDL3TTF::TextEngine, text : String) : LibSDL3::Text
+        LibSDL3TTF.create_text(engine.to_unsafe, to_unsafe, text, text.bytesize)
+      end
+
+      def font_wrap=(align : TTF_HorizontalAlignment)
+        LibSDL3TTF.set_font_wrap(to_unsafe, align)
+      end
+    end
+
+    class Text
+      @ptr : LibSDL3TTF::Text*
+
+      def initialize(@ptr : LibSDL3TTF::Text*)
+      end
+
+      def to_unsafe
+        @ptr
+      end
+
+      # TODO: move this to a SDL3::Text class
+      def draw(x : Float32, y : Float32) : Bool
+        LibSDL3TTF.draw_renderer_text(to_unsafe, x, y)
+      end
+
+      # TODO: move this to a SDL3::Text class
+      def wrap_whitespace_visible=(visible : Bool) : Bool
+        LibSDL3TTF.set_text_wrap_whitespace_visible(to_unsafe, visible)
+      end
+
+      def size : Tuple(Int32, Int32)
+        w = 0
+        h = 0
+
+        LibSDL3TTF.get_text_size(to_unsafe, pointerof(w), pointerof(h))
+
+        {w, h}
       end
     end
   end
